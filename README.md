70842b4299e7da3779e1a3dd3936aa5693edf6f8
Vanessa <vanessa.ys.cheung@gmail.com>
2023-04-05 22:59:24 -0400

    completed the fruit search autocomplete project

---
+++ README.md
@@ -0,0 +1,2 @@
+# Fruit Search AutoComplete Project!
+Built an Autofill Search Bar for various types of fruit. This project focused on DOM Manipulation, advanced array methods, and ES2015 features.
---
+++ script.js
@@ -0,0 +1,48 @@
+const input = document.querySelector('#fruit');
+const suggestions = document.querySelector('.suggestions ul');
+
+const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
+
+function search() {
+	let results = [];
+	const userInput = input.value.toLowerCase()
+
+	fruit.filter(function(e){
+		if (e.toLowerCase().includes(userInput) && input.value !== ""){
+			results.push(e)
+		}
+	});
+	
+	showSuggestions(results, userInput);
+}
+
+function removeElements() {
+	let shownSuggestions = document.querySelectorAll(".list-item")
+	shownSuggestions.forEach((item) => {
+		item.remove();
+	})
+}
+
+function showSuggestions(results, inputValue) {
+
+	results.forEach(function(result){
+
+		let startIdx = result.toLowerCase().indexOf(inputValue);
+		let endIdx = startIdx + inputValue.length;
+
+		const listItem = document.createElement("li");
+		listItem.classList.add("list-item");
+		listItem.innerHTML = result.substring(0, startIdx) + "<b>" + result.substring(startIdx, endIdx) + "</b>" + result.substring(endIdx, result.length);		
+		
+		suggestions.appendChild(listItem);
+	});
+}
+
+function useSuggestion(e) {
+	input.value = e.target.innerText;
+	removeElements();
+}
+
+input.addEventListener('keyup', removeElements);
+input.addEventListener('keyup', search)
+suggestions.addEventListener('click', useSuggestion);
