d0c7aa5751bc5fbc89cdcd0f198acf18554f82db
Vanessa <vanessa.ys.cheung@gmail.com>
2023-04-06 14:44:35 -0400

    fixed some bugs from last commit

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 const input = document.querySelector('#fruit');
-// const suggestions = document.querySelectorAll('.suggestions ul');
 const suggestionStartsWithInput = document.querySelector('.starts-with-input');
 const suggestionOthers = document.querySelector('.rest-of-suggestions');
 
-const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
+const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Rambutan', 'Raspberry', 'Redcurrant', 'Salak',  'Salmonberry', 'Satsuma', 'Soursop', 'Starfruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Tomato', 'Yuzu'];
 
 function search() {
 	let results = [];
@@ -33,7 +32,8 @@ function showSuggestions(results, inputValue) {
 
 		const listItem = document.createElement("li");
 		listItem.classList.add("list-item");
-		listItem.innerHTML = result.substring(0, startIdx) + "<b>" + result.substring(startIdx, endIdx) + "</b>" + result.substring(endIdx, result.length);		
+		listItem.innerHTML = result.substring(0, startIdx) + "<b>" + result.substring(startIdx, endIdx) + "</b>" + result.substring(endIdx, result.length);
+		listItem.onclick = useSuggestion;
 		
 		if (startIdx === 0) {
 			suggestionStartsWithInput.appendChild(listItem);
@@ -41,6 +41,8 @@ function showSuggestions(results, inputValue) {
 			suggestionOthers.appendChild(listItem);
 		}
 	});
+
+	// change focus to first item in ul
 }
 
 function useSuggestion(e) {
@@ -50,4 +52,3 @@ function useSuggestion(e) {
 
 input.addEventListener('keyup', removeElements);
 input.addEventListener('keyup', search)
-suggestions.addEventListener('click', useSuggestion);
